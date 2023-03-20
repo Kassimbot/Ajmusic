4755937151bcdffa19926227e374c65e56c2b9e3
Kassimbot <115211119+Kassimbot@users.noreply.github.com>
2023-03-20 22:35:11 +0530

    Update start.py

--- ShizukaXMusic/utils/inline/start.py
+++ ShizukaXMusic/utils/inline/start.py
@@ -67,14 +67,14 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         buttons.append(
             [
                 InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
-                InlineKeyboardButton(text=_["S_B_6"], url=f"https://t.me/darlzzzbots"),
+                InlineKeyboardButton(text=_["S_B_6"], url=f"https://t.me/darlzbots"),
             ]
         )
     else:
         if GITHUB_REPO:
             buttons.append(
                 [
-                    InlineKeyboardButton(text=_["S_B_6"], url=f"https://t.me/darlzzzbots"),
+                    InlineKeyboardButton(text=_["S_B_6"], url=f"https://t.me/darlzbots"),
                 ]
             )
         if OWNER:
